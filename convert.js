2874d77bcbbeeb30941c4d6d53564cd57104783a
Zack Elia <github@zacharyelia.com>
2020-07-25 19:20:14 -0400

    Add JavaScript files

---
+++ convert.js
@@ -0,0 +1,72 @@
+// Adapted from: https://gist.github.com/mjackson/5311256
+
+/**
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+ */
+module.exports.rgbToHsl = function (r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [ h, s, l ];
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+module.exports.hslToRgb =function(h, s, v) {
+  var r, g, b, i, f, p, q, t;
+
+  h /= 360;
+  s /= 100;
+  v /= 100;
+
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+      case 0: r = v; g = t; b = p; break;
+      case 1: r = q; g = v; b = p; break;
+      case 2: r = p; g = v; b = t; break;
+      case 3: r = p; g = q; b = v; break;
+      case 4: r = t; g = p; b = v; break;
+      case 5: r = v; g = p; b = q; break;
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+};
---
+++ index.js
@@ -0,0 +1,184 @@
+var Service;
+var Characteristic;
+var HomebridgeAPI;
+var convert = require("./convert");
+var noble = require('noble');
+
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("homebridge-gems-bulb", "GemsBulb", GemsBulb);
+};
+
+function GemsBulb(log, config) {
+  this.log = log
+  this.config = config
+
+  this.handle = 37
+
+  this.state = true
+  this.hue = 0
+  this.saturation = 100
+  this.brightness = 100
+
+  this.findBulb(this.config.mac);
+
+  this.informationService = new Service.AccessoryInformation();
+
+  this.informationService
+      .setCharacteristic(Characteristic.Manufacturer, "BEKEN")
+      .setCharacteristic(Characteristic.Model, "GEMS Smart LED Light Bulb")
+      .setCharacteristic(Characteristic.SerialNumber, "123-45-6789");
+
+  this.service = new Service.Lightbulb(this.config.name);
+
+  this.service.getCharacteristic(Characteristic.On)
+      .on('get', this.getState.bind(this));
+  this.service.getCharacteristic(Characteristic.On)
+      .on('set', this.setState.bind(this));
+
+  this.service.getCharacteristic(Characteristic.Hue)
+      .on('get', this.getHue.bind(this));
+  this.service.getCharacteristic(Characteristic.Hue)
+      .on('set', this.setHue.bind(this));
+
+  this.service.getCharacteristic(Characteristic.Saturation)
+      .on('get', this.getSaturation.bind(this));
+  this.service.getCharacteristic(Characteristic.Saturation)
+      .on('set', this.setSaturation.bind(this));
+
+  this.service.getCharacteristic(Characteristic.Brightness)
+      .on('get', this.getBrightness.bind(this));
+  this.service.getCharacteristic(Characteristic.Brightness)
+      .on('set', this.setBrightness.bind(this));
+}
+
+GemsBulb.prototype.findBulb = function(mac, callback) {
+  var that = this;
+  noble.on('stateChange', function(state) {
+    if (state === 'poweredOn') {
+        noble.startScanning();
+    } else {
+        noble.stopScanning();
+    }
+  });
+  noble.on('discover', function(peripheral) {
+    mac = mac.toLowerCase()
+    if (peripheral.id === mac || peripheral.address === mac) {
+        that.log("found my bulb");
+        that.peripheral = peripheral;
+    }
+  });
+}
+
+GemsBulb.prototype.attemptConnect = function(callback){
+  var buffer = new Buffer([0x6d, 0x6b, 0x3a, 0x30, 0x30, 0x30, 0x30])
+
+  if (this.peripheral && this.peripheral.state == "connected") {
+    this.peripheral.writeHandle(this.handle, buffer, true, function (error) {
+      callback(true);
+    });
+  } else if (this.peripheral && this.peripheral.state == "disconnected") {
+      this.log("lost connection to bulb. attempting reconnect ...");
+      var that = this;
+      this.peripheral.connect(function(error) {
+          if (!error) {
+              that.log("reconnect was successful");
+              that.peripheral.writeHandle(that.handle, buffer, true, function (error) {
+                callback(true);
+              });
+           } else {
+              that.log("reconnect was unsuccessful");
+              callback(false);
+          }
+      });
+  }
+}
+
+GemsBulb.prototype.updateColor = function(callback) {
+  var that = this;
+  var temp = function(res) {
+    if (!that.peripheral || !res) {
+      callback(new Error());
+      return;
+    }
+    var buffer = [0x63, 0x74, 0x6c, 0x3a, 0x31, 0x3a]
+    var rgb = convert.hslToRgb(that.hue, that.saturation, that.brightness);
+
+    for (const color of rgb) {
+      for (const char of color.toString()) {
+        buffer.push(char.charCodeAt(0));
+      }
+      buffer.push(0x3a);
+    }
+
+    buffer = new Buffer(buffer);
+    that.peripheral.writeHandle(that.handle, buffer, true, function (error) {
+      if (error) that.log('BLE: Write handle Error: ' + error);
+      callback();
+    });
+  }
+  this.attemptConnect(temp);
+}
+
+GemsBulb.prototype.setState = function(status, callback) {
+  var that = this;
+  var temp = function(res) {
+      if (!that.peripheral || !res) {
+          callback(new Error());
+          return;
+      }
+      var buffer = new Buffer([0x63, 0x6c, 0x6f, 0x73, 0x65])
+      if (status) {
+        buffer = new Buffer([0x6f, 0x70, 0x65, 0x6e])
+      }
+
+      that.peripheral.writeHandle(that.handle, buffer, true, function (error) {
+          if (error) that.log('BLE: Write handle Error: ' + error);
+          callback();
+      });
+  };
+  this.attemptConnect(temp);
+  this.state = status;
+};
+
+GemsBulb.prototype.getState = function(callback) {
+  callback(null, this.state);
+};
+
+GemsBulb.prototype.setHue = function(hue, callback) {
+  this.hue = hue
+  this.updateColor(function () {
+    callback();
+  });
+}
+
+GemsBulb.prototype.getHue = function(callback) {
+  callback(null, this.hue);
+}
+
+GemsBulb.prototype.setSaturation = function(saturation, callback) {
+  this.saturation = saturation
+  this.updateColor(function () {
+    callback();
+  });
+}
+
+GemsBulb.prototype.getSaturation = function(callback) {
+  callback(null, this.brightness);
+}
+
+GemsBulb.prototype.setBrightness = function(brightness, callback) {
+  this.brightness = brightness
+  this.updateColor(function () {
+    callback();
+  });
+}
+
+GemsBulb.prototype.getBrightness = function(callback) {
+  callback(null, this.brightness);
+}
+
+GemsBulb.prototype.getServices = function() {
+  return [this.informationService, this.service];
+};
